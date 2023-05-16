8e9194ced9bdcb3cfcb86b61fbfa74e9e5343c91
Дмитрий Земсков <zemsckov.dmitry@yandex.ru>
2023-05-16 17:13:51 +0300

    Код изначально

---
+++ Лабораторные АиСД (весна)/ClassDima.h
@@ -0,0 +1,7 @@
+#pragma once
+class ClassDima
+{
+public:
+	double ClasstoDima(double x, char oper, double y);
+};
+
---
+++ Лабораторные АиСД (весна)/Лабораторная 1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "ClassDima.h"
+
+using namespace std;
+
+int main()
+{
+	double x = 0.0; // Рекомендуется всегда присваивать им некоторые начальные значения
+	double y = 0.0; // Рекомендуется всегда присваивать им некоторые начальные значения
+	double result = 0.0; // Рекомендуется всегда присваивать им некоторые начальные значения
+	char oper = '+'; // Рекомендуется всегда присваивать им некоторые начальные значения
+
+	cout << "Calculator Console Application" << endl << endl;
+	cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b" << endl;
+
+	ClassDima c;
+	while (true)
+	{
+		cin >> x >> oper >> y;
+		if (oper == '/' && y == 0)
+		{
+			cout << "Division by 0 exception" << endl;
+			continue;
+		}
+		else
+		{
+			result = c.ClasstoDima(x, oper, y);
+		}
+		cout << "Result is: " << result << endl;
+	}
+
+	return 0;
+}
